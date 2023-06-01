5fdae6d7e1cb8e5ed5efc5b5273f7f78a3f5261f
gustigiyus <gustiggt123@gmail.com>
2023-06-01 19:27:36 +0700

    Customer and vendor registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 
 # Configure admin panel views.
 class CustomUserAdmin(UserAdmin):
-    list_display = ("email", "first_name", "last_name", "username", "role", "is_admin")
+    list_display = ("email", "first_name", "last_name", "username", "role", "is_active")
     ordering = ("-date_joined", )
     filter_horizontal = ()
     list_filter = ()
